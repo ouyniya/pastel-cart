f5622187aa4d022dbfc8fe650174a1dc494129a3
niya-s <niya.somkerd@gmail.com>
2025-07-10 21:20:19 +0700

    feat(product): add router and controller for product path

---
+++ controllers/product-controllers.js
@@ -0,0 +1,297 @@
+const prisma = require("../configs/prisma");
+const cloudinary = require("../configs/cloudinary");
+const createError = require("../utils/create-error");
+
+exports.createProduct = async (req, res) => {
+  try {
+    const { title, description, price, quantity, categoryId, images } =
+      req.body;
+
+    const product = await prisma.product.create({
+      data: {
+        title: title,
+        description: description,
+        price: parseFloat(price),
+        quantity: parseInt(quantity),
+        categoryId: parseInt(categoryId),
+        images: {
+          create: images.map((item) => ({
+            asset_id: item.asset_id,
+            public_id: item.public_id,
+            url: item.url,
+            secure_url: item.secure_url,
+          })),
+        },
+      },
+    });
+    res.json(product);
+  } catch (error) {
+    console.log("Create product error: ", error);
+    createError(500, "Internal server error");
+  }
+};
+
+exports.productList = async (req, res) => {
+  try {
+    const { page, limit } = req.params;
+    const skip = limit * (page - 1);
+
+    const products = await prisma.product.findMany({
+      take: parseInt(limit),
+      skip: parseInt(skip),
+      orderBy: { createdAt: desc },
+      include: {
+        category: true,
+        images: true,
+      },
+    });
+
+    if (!products) {
+      return createError(404, "Products not found");
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.log("Get product list error: ", error);
+    createError(500, "Internal server error");
+  }
+};
+
+exports.getProducts = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await prisma.product.findFirst({
+      where: {
+        id,
+      },
+      include: {
+        category: true,
+        images: true,
+      },
+    });
+
+    if (!product) {
+      return createError(404, "Product not found");
+    }
+
+    res.json(product);
+  } catch (error) {
+    console.log("Get product by id error: ", error);
+    createError(500, "Internal server error");
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, price, quantity, categoryId, images } =
+      req.body;
+
+    const isProductExist = await prisma.product.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!isProductExist) {
+      return createError(404, "Product not found");
+    }
+
+    const product = await prisma.product.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description,
+        price: parseFloat(price),
+        quantity: parseInt(quantity),
+        categoryId,
+        images: {
+          create: images.map((item) => ({
+            asset_id: item.asset_id,
+            public_id: item.public_id,
+            url: item.url,
+            secure_url: item.secure_url,
+          })),
+        },
+      },
+    });
+
+    res.json(product);
+  } catch (error) {
+    console.log("Update product error: ", error);
+    createError(500, "Internal server error");
+  }
+};
+
+exports.removeProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const isProductExist = await prisma.product.findFirst({ where: id });
+
+    if (!isProductExist) {
+      return createError(404, "Product not found");
+    }
+
+    await prisma.product.delete({
+      where: {
+        id,
+      },
+    });
+    res.json("Remove product successfully");
+  } catch (error) {
+    console.log("Remove product error :", error);
+    createError(500, "Internal server error");
+  }
+};
+
+exports.listProductBy = async (req, res) => {
+  try {
+    const { sort, order, limit } = req.body;
+
+    const products = await prisma.product.findMany({
+      take: limit,
+      orderBy: {
+        [sort]: order,
+      },
+      include: {
+        category: true,
+        images: true,
+      },
+    });
+
+    if (!products) {
+      return createError(404, "Products not found");
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.log("Sort product error: ", error);
+    createError(500, "Internal server error");
+  }
+};
+
+exports.searchProductByName = async (req, res, query) => {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      include: {
+        category: true,
+        images: true,
+      },
+    });
+
+    if (!products) {
+      return createError(404, "Products not found");
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.log("Search product by name error: ", error);
+    createError(500, "Internal server error");
+  }
+};
+
+const searchProductByPrice = async (req, res, priceRange) => {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        price: {
+          gte: Number(priceRange[0]),
+          lte: Number(priceRange[1]),
+        },
+      },
+      include: {
+        category: true,
+        images: true,
+      },
+    });
+
+    if (!products) {
+      return createError(404, "Products not found");
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.log("Search product by price error: ", error);
+    createError(500, "Internal server error");
+  }
+};
+
+const searchProductByCategory = async (req, res, categoryId) => {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        categoryId: {
+          in: categoryId.map((id) => id),
+        },
+      },
+      include: {
+        category: true,
+        images: true,
+      },
+    });
+
+    if (!products) {
+      return createError(404, "Products not found");
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.log("Search product by category error: ", error);
+    createError(500, "Internal server error");
+  }
+};
+
+exports.searchFilter = async (req, res) => {
+  try {
+    const { query, category, price } = req.body;
+
+    if (query) {
+      await searchProductByName(req, res, query);
+    }
+    if (category) {
+      await searchProductByCategory(req, res, category);
+    }
+    if (price) {
+      await searchProductByPrice(req, res, price);
+    }
+  } catch (error) {
+    console.log("Search filter error: ", error);
+    createError(500, "Internal server error");
+  }
+};
+
+exports.addImages = async (req, res) => {
+  try {
+    const result = await cloudinary.uploader.upload(req.body.image, {
+      public_id: `image-nysdev-${Date.now()}`,
+      resource_type: "auto",
+      folder: "shopping",
+    });
+
+    res.json(result);
+  } catch (error) {
+    console.log("Search filter error: ", error);
+    createError(500, "Internal server error");
+  }
+};
+
+exports.removeImages = async (req, res) => {
+  try {
+    const { public_id } = req.body;
+
+    cloudinary.uploader.destroy(public_id, (result) => {
+      res.json("Remove Image Successfully");
+    });
+  } catch (error) {
+    console.log("Remove images error: ", error);
+    createError(500, "Internal server error");
+  }
+};
--- routes/product-routes.js
+++ routes/product-routes.js
@@ -1,5 +1,26 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
+const {
+  createProduct,
+  productList,
+  removeProduct,
+  listProductBy,
+  searchFilter,
+  addImages,
+  removeImages,
+} = require("../controllers/product-controllers");
+const { authCheck, adminCheck } = require("../middlewares/authCheck");
 
-router.post('/product')
+router.post("/product", createProduct);
+router.get("/products/:page/:limit", productList);
+router.get("/product/:id", getProducts);
+router.put("/product/:id", updateProduct);
+router.delete("/product/:id", removeProduct);
+router.post("/product-by", listProductBy);
+router.post("/search/filters", searchFilter);
+
+router.post("/images", authCheck, adminCheck, addImages);
+router.post("/remove-image", authCheck, adminCheck, removeImages);
+
+module.exports = router;
